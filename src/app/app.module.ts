ab096caf93babf362bb428c14a3182364bf0aca3
MaHeak <maheak.demons@gmail.com>
2017-09-11 08:29:46 +0700

    Adding top and side nav menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SidebarmenuComponent } from './sidebarmenu/sidebarmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidebarmenuComponent
   ],
   imports: [
     BrowserModule
